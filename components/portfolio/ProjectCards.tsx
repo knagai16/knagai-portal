c1ddc624671547b17ebd338fc4b5199fd8409ed1
knagai16 <knagai1616@gmail.com>
2025-02-13 23:17:24 +0900

    Add real portfolio

--- components/portfolio/ProjectCards.tsx
+++ components/portfolio/ProjectCards.tsx
@@ -9,27 +9,39 @@ export interface Project {
 };
 
 const projects: Project[] = [
-  {
-      title: "ポートフォリオサイト",
-      period: "2025年2月〜現在",
-      role: "個人開発",
-      technologies: [
-        "React",
-        "TypeScript",
-      ],
+    {
+      title: "くじ引きサービス",
+      period: "2018年6月〜2021年11月",
+      role: "バックエンドエンジニア",
+      technologies: ["Java", "Spring Boot", "Kubernetes"],
       description:
-        "個人の活動でポートフォリオサイトを作成・保守運用を担当。React + TypeScript を用いて開発。",
+        "くじ引きサービスの新機能開発・運用を担当。Spring Boot（Java）を用いた API 設計・開発や、Kubernetesを活用したマイクロサービスの運用を行う。",
     },
     {
-        title: "業務サイト",
-        period: "2020年1月〜現在",
-        role: "バックエンドエンジニア",
+      title: "クラウド会計ソフト",
+      period: "2021年12月〜2025年2月",
+      role: "フルスタックエンジニア",
+      technologies: [
+        "Ruby",
+        "React",
+        "TypeScript",
+        "AWS EKS",
+        "AWS Lambda",
+        "AWS S3",
+      ],
+      description:
+        "クラウド会計ソフトの新機能開発・保守運用を担当。Ruby on Rails、 React + TypeScript を用いた開発、gRPCを用いたマイクロサービス構築を行う。",
+    },
+    {
+        title: "ポートフォリオサイト",
+        period: "2025年2月〜現在",
+        role: "個人開発",
         technologies: [
-          "Ruby",
-          "Kubernetes",
+          "React",
+          "TypeScript",
         ],
         description:
-          "XX社の業務サイトのバックエンド開発をRuby on Railsで担当。またKubernetesでのデプロイも担当。",
+          "個人の活動でポートフォリオサイトを作成・保守運用を担当。React + TypeScript を用いて開発。",
       },
   ];
 
